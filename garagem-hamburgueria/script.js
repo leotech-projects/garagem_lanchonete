dcd77c68edfd7f4993342e374ee8363833c5b6e6
Leonardo Silva <150615719+Leotech47@users.noreply.github.com>
2025-02-04 17:23:04 -0300

    Update script.js

--- garagem-hamburgueria/script.js
+++ garagem-hamburgueria/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("Garagem Hamburgueria - Site carregado!");
+    console.log("Garagem Hamburgueria JF - Site carregado!");
     
     // Exemplo de interatividade: alerta ao clicar no botão do cardápio
     document.querySelector(".btn").addEventListener("click", function () {
